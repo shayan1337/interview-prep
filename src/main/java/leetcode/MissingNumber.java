377696de31ff0821501a14be1fe0a4c88114b708
Sayan <shayan1337@gmail.com>
2020-05-26 18:58:32 +0530

    add solution for missing number problem

---
+++ src/main/java/leetcode/MissingNumber.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+/**
+ * 268. Missing Number (https://leetcode.com/problems/missing-number/)
+ */
+public class MissingNumber {
+    public static int missingNumber(int[] nums) {
+        boolean[] isPresent = new boolean[nums.length + 1];
+
+        for(int n:nums) {
+            isPresent[n] = true;
+        }
+
+        for(int i=0; i<isPresent.length; i++) {
+            if (!isPresent[i])
+                return i;
+        }
+
+        return -1;
+    }
+
+    /**
+     * The other way of finding the missing number using AP
+     * @param nums int[]
+     * @return int
+     */
+    public int missingNumberAP(int[] nums) {
+        int expectedSum = (nums.length*(nums.length+1))/2;
+        int actualSum = 0;
+
+        for(int n:nums) {
+            actualSum += n;
+        }
+
+        return expectedSum - actualSum;
+    }
+}
---
+++ src/test/java/leetcode/MissingNumberTest.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+class MissingNumberTest {
+
+    @Test
+    void testMissingNumber() {
+        int res = MissingNumber.missingNumber(new int[]{5,2,0,3,4});
+        Assertions.assertEquals(1, res);
+    }
+
+}
