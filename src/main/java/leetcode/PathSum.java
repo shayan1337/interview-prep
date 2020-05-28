3562ec0791b499ed68ed75567578edd6a2be8c3c
Sayan <shayan1337@gmail.com>
2020-05-28 19:52:50 +0530

    add solution for path sum leetcode problem

---
+++ src/main/java/leetcode/PathSum.java
@@ -0,0 +1,46 @@
+package leetcode;
+
+/**
+ * 112. Path Sum -> (https://leetcode.com/problems/path-sum/)
+ */
+
+public class PathSum {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null)
+            return false;
+        return hasPathSum(root, sum, 0);
+    }
+
+    public boolean hasPathSum(TreeNode root, int sum, int currentSum) {
+        if (root.left == null && root.right == null)
+            return (currentSum + root.val) == sum;
+
+        boolean hasLeftPath = false;
+
+        if(root.left != null) {
+            hasLeftPath = hasPathSum(root.left, sum, currentSum + root.val);
+        }
+
+        boolean hasRightPath = false;
+
+        if(root.right != null) {
+            hasRightPath = hasPathSum(root.right, sum, currentSum + root.val);
+        }
+
+        return hasLeftPath || hasRightPath;
+    }
+}
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
