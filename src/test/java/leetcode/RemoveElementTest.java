5e22bf5390b1a3d04e62a1a027a88015d2769f24
Sayan <shayan1337@gmail.com>
2020-05-28 22:16:23 +0530

    add solution to leetcode remove element problem

---
+++ src/test/java/leetcode/RemoveElementTest.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class RemoveElementTest {
+
+    @Test
+    void testRemoveElementShouldRemoveElement1() {
+        int[] nums = {3, 2, 2, 3};
+        int res = RemoveElement.removeElement(nums, 3);
+
+        Assertions.assertEquals(2, res);
+    }
+
+    @Test
+    void testRemoveElementShouldRemoveElement2() {
+        int[] nums = {1};
+        int res = RemoveElement.removeElement(nums, 1);
+
+        Assertions.assertEquals(0, res);
+    }
+
+}
