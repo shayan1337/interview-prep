ca763e75176f992c2f5e870b88e53518bb8807b8
Sayan <shayan1337@gmail.com>
2020-05-30 21:02:13 +0530

    add circular linked list implementation

--- src/main/java/linkedlist/CircularLinkedList.java
+++ src/main/java/linkedlist/CircularLinkedList.java
@@ -1,4 +1,139 @@
 package linkedlist;
 
+
+/**
+ * Represents a circular linked list where the last element points to the head of the list
+ * APIs include :
+ * 1) add(int) -> adds value to the head of the list
+ * 2) remove() -> removes element from the head of the list
+ * 3) insert(int, int) -> adds element at a particular position in the list
+ */
 public class CircularLinkedList {
+
+    static class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int numberOfElements = 0;
+
+
+    /**
+     * Add an element to the head of the list
+     * @param data is the data to be inserted at the head of the list
+     */
+    public void add(int data) {
+        Node newNode = new Node(data);
+
+        if (numberOfElements == 0) {
+            head = newNode;
+            tail = newNode;
+            numberOfElements++;
+            return;
+        }
+
+        newNode.next = head;
+        head = newNode;
+        tail.next = head;
+        numberOfElements++;
+    }
+
+
+    /**
+     * Remove element from the head of the list
+     * @return <tt>true</tt> if element is deleted; <tt>false</tt> otherwise
+     */
+    public boolean remove() {
+        if (numberOfElements == 0)
+            return false;
+
+        if (numberOfElements == 1) {
+            head = null;
+            tail = null;
+            numberOfElements--;
+            return true;
+        }
+
+        head = head.next;
+        tail.next = head;
+        numberOfElements--;
+        return true;
+    }
+
+
+    /**
+     * Inserts element at a particular position in the list
+     * @param data is the data to be inserted
+     * @param pos is the position in which the new element is to be inserted
+     */
+    public void insert(int data, int pos) {
+        if (pos < 0 || pos > numberOfElements)
+            return;
+
+        Node current = head;
+        Node previous = head;
+        int currentPos = 0;
+        boolean tailTraversed = false;
+
+        while(current != null && currentPos < pos && !tailTraversed) {
+            previous = current;
+            current = current.next;
+            currentPos++;
+
+            tailTraversed = previous == tail;
+        }
+
+        if ((current == head && !tailTraversed) || current == null) {
+            add(data);
+            return;
+        }
+
+        if (tailTraversed) {
+            addToTail(data);
+            return;
+        }
+
+        Node newNode = new Node(data);
+
+        newNode.next = current;
+        previous.next = newNode;
+        numberOfElements++;
+    }
+
+    private void addToTail(int data) {
+        Node newNode = new Node(data);
+        tail.next = newNode;
+        newNode.next = head;
+        tail = newNode;
+        numberOfElements++;
+    }
+
+
+    public int[] values() {
+        int[] values = new int[numberOfElements];
+        int index = 0;
+
+        Node current = head;
+        boolean tailTraversed = false;
+
+        while(current != null && !tailTraversed) {
+            values[index] = current.data;
+            index++;
+
+            tailTraversed = current == tail;
+            current = current.next;
+        }
+
+        return values;
+    }
+
+
+    public int size() {
+        return  numberOfElements;
+    }
 }
--- src/test/java/linkedlist/CircularLinkedListTest.java
+++ src/test/java/linkedlist/CircularLinkedListTest.java
@@ -1,7 +1,125 @@
 package linkedlist;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CircularLinkedListTest {
 
+    @Test
+    void testAddShouldAddElementsToTheList() {
+        CircularLinkedList circularLinkedList = new CircularLinkedList();
+
+        circularLinkedList.add(1);
+        circularLinkedList.add(2);
+        circularLinkedList.add(3);
+        circularLinkedList.add(4);
+
+        assertEquals(4, circularLinkedList.size());
+        assertArrayEquals(new int[]{4, 3, 2, 1}, circularLinkedList.values());
+    }
+
+    @Test
+    void testRemoveShouldReturnFalseForEmptyList() {
+        CircularLinkedList circularLinkedList = new CircularLinkedList();
+
+        boolean res = circularLinkedList.remove();
+
+        assertFalse(res);
+        assertEquals(0, circularLinkedList.size());
+        assertArrayEquals(new int[]{}, circularLinkedList.values());
+    }
+
+    @Test
+    void testRemoveShouldRemoveTheOnlyElementInTheList() {
+        CircularLinkedList circularLinkedList = new CircularLinkedList();
+        circularLinkedList.add(1);
+
+        boolean res = circularLinkedList.remove();
+
+        assertTrue(res);
+        assertEquals(0, circularLinkedList.size());
+        assertArrayEquals(new int[]{}, circularLinkedList.values());
+    }
+
+    @Test
+    void testRemoveShouldRemoveElementFromHeadInTheList() {
+        CircularLinkedList circularLinkedList = new CircularLinkedList();
+        circularLinkedList.add(1);
+        circularLinkedList.add(2);
+        circularLinkedList.add(3);
+
+        boolean res = circularLinkedList.remove();
+
+        assertTrue(res);
+        assertEquals(2, circularLinkedList.size());
+        assertArrayEquals(new int[]{2, 1}, circularLinkedList.values());
+    }
+
+    @Test
+    void testRemoveShouldRemoveMultipleElementsFromHeadInTheList() {
+        CircularLinkedList circularLinkedList = new CircularLinkedList();
+        circularLinkedList.add(1);
+        circularLinkedList.add(2);
+        circularLinkedList.add(3);
+
+        circularLinkedList.remove();
+        boolean res = circularLinkedList.remove();
+
+        assertTrue(res);
+        assertEquals(1, circularLinkedList.size());
+        assertArrayEquals(new int[]{1}, circularLinkedList.values());
+    }
+
+    @Test
+    void testRemoveShouldRemoveAllElementsFromTheList() {
+        CircularLinkedList circularLinkedList = new CircularLinkedList();
+        circularLinkedList.add(1);
+        circularLinkedList.add(2);
+
+        circularLinkedList.remove();
+        boolean res = circularLinkedList.remove();
+
+        assertTrue(res);
+        assertEquals(0, circularLinkedList.size());
+        assertArrayEquals(new int[]{}, circularLinkedList.values());
+    }
+
+    @Test
+    void testShouldInsertElementAtAppropriatePosition() {
+        CircularLinkedList circularLinkedList = new CircularLinkedList();
+
+        circularLinkedList.insert(1, 0);
+
+        assertEquals(1, circularLinkedList.size());
+        assertArrayEquals(new int[]{1}, circularLinkedList.values());
+    }
+
+    @Test
+    void testShouldInsertElementAtTheEndOfTheList() {
+        CircularLinkedList circularLinkedList = new CircularLinkedList();
+        circularLinkedList.add(1);
+        circularLinkedList.add(2);
+        circularLinkedList.add(3);
+
+        circularLinkedList.insert(4, 3);
+
+        assertEquals(4, circularLinkedList.size());
+        assertArrayEquals(new int[]{3, 2, 1, 4}, circularLinkedList.values());
+    }
+
+    @Test
+    void testShouldInsertMultipleElementsAtTheEndOfTheList() {
+        CircularLinkedList circularLinkedList = new CircularLinkedList();
+        circularLinkedList.add(1);
+        circularLinkedList.add(2);
+        circularLinkedList.add(3);
+
+        circularLinkedList.insert(4, 3);
+        circularLinkedList.insert(5, 4);
+
+        assertEquals(5, circularLinkedList.size());
+        assertArrayEquals(new int[]{3, 2, 1, 4, 5}, circularLinkedList.values());
+    }
 }
