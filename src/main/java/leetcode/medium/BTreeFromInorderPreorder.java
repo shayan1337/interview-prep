f1a8bb3ab452119941c7984b238351105c149803
Sayan <shayan1337@gmail.com>
2020-06-11 11:49:55 +0530

    update for optimised solution of leetcode problem 'contruct BT from preorder and inorder traversal'

--- src/main/java/leetcode/medium/BTreeFromInorderPreorder.java
+++ src/main/java/leetcode/medium/BTreeFromInorderPreorder.java
@@ -1,9 +1,7 @@
 package leetcode.medium;
 
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
-import java.util.Queue;
 
 /**
  * 105. Construct Binary Tree from Preorder and Inorder Traversal (https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/)
@@ -17,30 +15,27 @@ public class BTreeFromInorderPreorder {
         if (inorder.length != preorder.length)
             return null;
 
-        Queue<Integer> q = new LinkedList<>();
-        for(int n : preorder)
-            q.add(n);
-
         Map<Integer, Integer> map = new HashMap<>();
 
-        for (int i=0; i<inorder.length; i++)
+        for (int i = 0; i < inorder.length; i++)
             map.put(inorder[i], i);
 
-        return buildTree(q, map, 0, inorder.length - 1);
+        return buildTree(preorder, 0, map, 0, inorder.length - 1);
     }
 
-    private static TreeNode buildTree(Queue<Integer> q, Map<Integer, Integer> map, int iStart, int iEnd) {
+    private static TreeNode buildTree(int[] preorder, int preorderIndex, Map<Integer, Integer> map, int iStart, int iEnd) {
         if (iStart > iEnd)
             return null;
 
-        TreeNode node = new TreeNode(q.poll());
+        TreeNode node = new TreeNode(preorder[preorderIndex]);
 
         if (iStart == iEnd)
             return node;
 
         int indexInInOrder = map.get(node.val);
-        node.left = buildTree(q, map, iStart, indexInInOrder - 1);
-        node.right = buildTree(q, map, indexInInOrder + 1, iEnd);
+        int leftCount = indexInInOrder - iStart;
+        node.left = buildTree(preorder, preorderIndex + 1, map, iStart, indexInInOrder - 1);
+        node.right = buildTree(preorder, preorderIndex + leftCount + 1, map, indexInInOrder + 1, iEnd);
         return node;
     }
 }
