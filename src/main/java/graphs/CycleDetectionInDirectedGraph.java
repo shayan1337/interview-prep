b5e5436cee82bd50d56fa640678bb7574c6fbe3e
Sayan Chakraborty <shayan1337@gmail.com>
2021-01-19 13:00:43 +0530

    add code for cycle detection in directed graph

---
+++ src/main/java/graphs/CycleDetectionInDirectedGraph.java
@@ -0,0 +1,50 @@
+package graphs;
+
+import java.util.*;
+
+// Graph could be disconnected
+public class CycleDetectionInDirectedGraph {
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+
+        Map<Integer, List<Integer>> dependencies = new HashMap<>();
+
+        for (int[] arr : prerequisites) {
+            if (!dependencies.containsKey(arr[0]))
+                dependencies.put(arr[0], new ArrayList<>());
+            dependencies.get(arr[0]).add(arr[1]);
+        }
+
+        return isCyclic(dependencies, numCourses);
+    }
+
+    private boolean isCyclic(Map<Integer, List<Integer>> dependencies, int n) {
+        HashSet<Integer> visited = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            if (isCyclicUtil(i, dependencies, visited, new HashSet<>()))
+                return true;
+        }
+
+        return false;
+    }
+
+    private boolean isCyclicUtil(int i, Map<Integer, List<Integer>> dep, Set<Integer> visited, Set<Integer> nodesVisitedOnCurrentTraversal) {
+        if (nodesVisitedOnCurrentTraversal.contains(i))
+            return true;
+
+        if (visited.contains(i))
+            return false;
+
+        nodesVisitedOnCurrentTraversal.add(i);
+        visited.add(i);
+
+        for (int n : dep.getOrDefault(i, new ArrayList<>())) {
+            if (isCyclicUtil(n, dep, visited, nodesVisitedOnCurrentTraversal))
+                return true;
+        }
+
+        nodesVisitedOnCurrentTraversal.remove(i);
+        return false;
+    }
+}
